722ab38320b9494f3069ca83e747c51762ff56c6
Tyler Reardon <tdr.reardon@gmail.com>
2018-12-08 23:56:54 -0800

    Router organization

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,25 @@
 import Pages from './pages'
 
-export default [
-  {
-    navName: 'Login',
+export const Links = {
+  // internal links
+  login: {
     name: 'login',
     path: '/login',
     component: Pages.Login
   },
-  {
-    navName: 'Google',
+  // external links
+  google: {
     name: 'google',
     path: 'https://www.google.com'
   }
+}
+
+// Routes for the main router
+const Main = [
+  {
+    navName: 'Login',
+    ...Links.login
+  }
 ]
+
+export default Main
